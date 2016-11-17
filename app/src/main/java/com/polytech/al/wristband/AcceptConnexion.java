9819fc1916519a159e6f633d94ce49c1629edf83
Nicolas <lecourtoisn@gmail.com>
2016-11-18 00:22:58 +0100

    Colours in addition to vibrations. Such colour. Much fancy. Wow !

--- app/src/main/java/com/polytech/al/wristband/AcceptConnexion.java
+++ app/src/main/java/com/polytech/al/wristband/AcceptConnexion.java
@@ -1,23 +1,20 @@
 package com.polytech.al.wristband;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Vibrator;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Random;
 import java.util.Timer;
-import java.util.TimerTask;
 import java.util.UUID;
 
 public class AcceptConnexion extends Thread {
-    private Activity activity;
+    private MainActivity activity;
     private BluetoothServerSocket serverSocket;
     private BluetoothSocket socket = null;
     private BluetoothAdapter adapter;
@@ -25,7 +22,7 @@ public class AcceptConnexion extends Thread {
     private InputStream inputStream = null;
     private Timer timer = new Timer();
 
-    public AcceptConnexion(Activity activity) {
+    public AcceptConnexion(MainActivity activity) {
         this.activity = activity;
         adapter = BluetoothAdapter.getDefaultAdapter();
         try {
@@ -43,15 +40,20 @@ public class AcceptConnexion extends Thread {
                 int read = inputStream.read(bytes);
                 byte code = bytes[0];
                 Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
-                if (code == 0) {
+                if (code == 1) {
                     System.out.println("Droite");
                     v.vibrate(100);
                     Thread.sleep(300);
                     v.vibrate(100);
-                } else {
+                    activity.setColor(Color.RED);
+                } else if (code == 2) {
                     System.out.println("Gauche");
                     v.vibrate(200);
+                    activity.setColor(Color.YELLOW);
+                } else {
+                    activity.setColor(Color.GREEN);
                 }
+                activity.handler.post(activity.changeColour);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
--- app/src/main/java/com/polytech/al/wristband/MainActivity.java
+++ app/src/main/java/com/polytech/al/wristband/MainActivity.java
@@ -1,89 +1,48 @@
 package com.polytech.al.wristband;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
 import java.util.UUID;
-import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 42;
+//    private static final int REQUEST_ENABLE_BT = 42;
+    private RelativeLayout RL;
     private static final UUID uuid = UUID.fromString("33d08e9e-ac0d-11e6-80f5-76304dec7eb7");
+
     private BluetoothSocket socket;
+    private int color;
+
+    public Handler handler = new Handler();
+
+     public Runnable changeColour = new Runnable() {
+        public void run() {
+            RL.setBackgroundColor(color);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RL = (RelativeLayout)findViewById(R.id.Layout);
 
-        new AcceptConnexion(this).run();
+        new AcceptConnexion(this).start();
+
     }
-//
-//        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//        if (!mBluetoothAdapter.isEnabled()) {
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//        }
-//
-//        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-//        BluetoothDevice phone = null;
-//        if (pairedDevices.size() > 0) {
-//            for (BluetoothDevice device : pairedDevices) {
-//                if (device.getAddress().equals("78:00:9E:18:71:5D")) {
-//                    phone = device;
-//                }
-//            }
-//        }
-//        assert(phone != null);
-//        try {
-//            socket = phone.createRfcommSocketToServiceRecord(uuid);
-//            System.out.println("Trying to connect");
-//            socket.connect();
-//            Toast.makeText(MainActivity.this, "Connected to " + socket.getRemoteDevice().getName(), Toast.LENGTH_SHORT).show();
-//
-//            InputStream inputStream = socket.getInputStream();
-//
-//            byte[] bytes = new byte[1];
-//            while (true) {
-//                inputStream.read(bytes);
-//                byte code = bytes[0];
-//
-//                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-////                View view = findViewById(R.id.screen);
-////                View root = view.getRootView();
-//
-//                int duration = (code == 0 ? 100 : 200);
-//                if (code == 0) {
-//                    System.out.println("Droite");
-//                    v.vibrate(100);
-//                    Thread.sleep(300);
-//                    v.vibrate(100);
-//                } else {
-//                    System.out.println("Gauche");
-//                    v.vibrate(200);
-//                }
-//
-////                root.setBackgroundColor(bgColor);
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
 
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
 }
